defb4b47378eac0ac21042c5eea6480086627850
Michael Lai <mike24567@gmail.com>
2016-09-27 09:40:45 -0400

    Solved filter functional programming question

--- fp.js
+++ fp.js
@@ -1,28 +1,38 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
+// var peoplesAges = people.map(function(p){
+//     var age = [];
+//     age = p.age;
+//     return age;
+//   })
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(person => {
-  peoplesAges.push(person.age)
-})
+// people.forEach(person => {
+//   peoplesAges.push(person.age)
+// })
+
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
+var peopleYoungerThan35 = people.filter(youngerThan35)
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(person => {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-})
+
+function youngerThan35(person){
+  return person.age <35;
+}
+
+// people.forEach(person => {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// })
 
 // Convert the following to use `.forEach`
 
