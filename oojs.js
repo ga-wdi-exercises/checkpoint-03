41e335963a1cb472b34e99609fab39383d51ad94
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-27 09:34:50 -0400

    edited

--- oojs.js
+++ oojs.js
@@ -7,24 +7,25 @@
 class Playlist {
   constructor(title){
     this.title = title;
-    this.songs = songs;
+    this.songs = [];
     this.favorites = 0;
   }
-  addSong(){
+  addSong(song){
     this.songs.push(song);
 
   }
 }
-// Create an instance of the Playlist class.
-const sing = new Playlist(loveSong)
-
-// Create an Album class that inherits from Playlist. It should also...
-  // Have an artist property that is determined by some input.
-
+// // Create an instance of the Playlist class.
+const sing = new Playlist("loveSong")
+//
+// // Create an Album class that inherits from Playlist. It should also...
+//   // Have an artist property that is determined by some input.
+//
 class Album extends Playlist{
   constructor(title,artist){
     this.artist = artist;
   }
 }
 
-// Create an instance of the Album class.
+// // Create an instance of the Album class.
+const good = new Album("Love Song", "Taylor Swift");
