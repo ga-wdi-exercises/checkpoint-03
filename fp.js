2b6a04d33213f774e7f5573057c2e5df30e35b02
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-27 09:43:06 -0400

    filter

--- fp.js
+++ fp.js
@@ -1,14 +1,14 @@
 // Convert the following code to use `.map`
 
-// var peoplesAges = []
-// var people = [
-//   {name: "Jane", age: 32},
-//   {name: "Jessica", age: 34},
-//   {name: "Jasmine", age: 39}
-// ]
-// var peoplesAges = people.map(person => {
-//   peoplesAges.push(person.age)
-// })
+var peoplesAges = []
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+var peoplesAges = people.map(person => {
+  peoplesAges.push(person.age)
+})
 
 
 // Convert the following code to use `.filter`
@@ -21,7 +21,7 @@ var people = [
 ]
 var peopleYoungerThan35 = people.filter(person => {
   if(person.age < 35){
-    peopleYoungerThan35.push(person)
+    peopleYoungerThan35.push(person.name)
   }
 })
 
