df7c1ffe395b6459cb4185ce886bd5f758891c7b
Nick Oki <nick.oki@live.com>
2016-09-27 09:15:37 -0400

    Added Album Class, inherits from Playlist Class

--- oojs.js
+++ oojs.js
@@ -1,16 +1,16 @@
 // Define a Playlist class. It should have...
 class Playlist {
 	constructor(title, song) {
-  // A title property that is determined by some input.
-	this.title = title
-  // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
-	let songArray = []
-	songArray.push(song)
-	this.songs = songArray
-  // A favorites property that is initialized at 0. This cannot be set by user input.
-	this.favorites = 0
+		// A title property that is determined by some input.
+		this.title = title
+		// A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
+		let songArray = []
+		songArray.push(song)
+		this.songs = songArray
+		// A favorites property that is initialized at 0. This cannot be set by user input.
+		this.favorites = 0
 	}
-  // An addSong method that adds a song to the songs property.
+	// An addSong method that adds a song to the songs property.
 	addSong(song) {
 		this.songs.push(song)
 	}
@@ -20,6 +20,12 @@ class Playlist {
 var playlist1 = new Playlist('WDI Jams', "Jesse's Girl")
 
 // Create an Album class that inherits from Playlist. It should also...
-  // Have an artist property that is determined by some input.
+class Album extends Playlist {
+	constructor(title, song, artist) {
+		super(title, song)
+		// Have an artist property that is determined by some input.
+		this.artist = artist
+	}
+}
 
 // Create an instance of the Album class.
