69aaded7c0e540230c0d15b80024bf3d01608e02
Michael Rubin <michael@rubinsworld.com>
2016-09-27 09:24:34 -0400

    Part 1 Completed

--- oojs.js
+++ oojs.js
@@ -4,9 +4,30 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
-// Create an instance of the Playlist class.
+  class Playlist {
+    constructor(title, songs) {
+      this.title = title;
+      this.songs = {};
+    }
+    addSong(name){
+      this.songs.push ({"name":name,"favorites":0});
+      return;
+    }/* addSong Method */
+  } /* class */
 
+
+// Create an instance of the Playlist class.
+const pl= new Playlist("mikes" );
 // Create an Album class that inherits from Playlist. It should also...
+class Album extends Playlist {
+  constructor(artist){
+    super();
+    this.artist =artist;
+  }
+
+}
+
   // Have an artist property that is determined by some input.
 
 // Create an instance of the Album class.
+const artist= new Album("Doobie Brothers" );
