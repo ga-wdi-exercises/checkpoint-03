10657706e9ba076def1eaf5efe6587620465f787
Nick Oki <nick.oki@live.com>
2016-09-27 09:11:07 -0400

    Created instance of Playlist Class

--- oojs.js
+++ oojs.js
@@ -17,6 +17,7 @@ class Playlist {
 }
 
 // Create an instance of the Playlist class.
+var playlist1 = new Playlist('WDI Jams', "Jesse's Girl")
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
