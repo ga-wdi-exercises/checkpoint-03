52c67419143b704c1903eb8b48a48a553e78e0e2
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-27 09:51:29 -0400

    submitting fp exercises

--- fp.js
+++ fp.js
@@ -1,14 +1,19 @@
 // Convert the following code to use `.map`
-
 var peoplesAges = []
+
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(person => {
-  peoplesAges.push(person.age)
-})
+
+// function peopleMap() {
+//   var peoplesAges = people.map(age);
+// }
+
+// people.forEach(person => {
+//   peoplesAges.push(person.age)
+// })
 
 // Convert the following code to use `.filter`
 
@@ -24,6 +29,18 @@ people.forEach(person => {
   }
 })
 
+function isYoungerThan(age) {
+  return people.age <35;
+  var peopleYoungerThan35 = people.filter(isYoungerThan);
+}
+
+
+// people.forEach(person => {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// })
+
 // Convert the following to use `.forEach`
 
 var people = [
@@ -31,4 +48,12 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(person => person.name.match("s"))
+
+peopleWhoseNamesHaveTheLetterS = []
+
+people.forEach(person => {
+  if (person.name.match("s")){
+    peopleWhoseNamesHaveTheLetterS.push(person)
+  }
+})
+// var peopleWhoseNamesHaveTheLetterS = people.filter(person => person.name.match("s"))
