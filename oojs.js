cf6b2c3b1dfe682a0315fd8dfc5f48cdfe4a01ba
Mark Sweet <marks828@gmail.com>
2016-09-27 09:33:45 -0400

    map functionality

--- oojs.js
+++ oojs.js
@@ -6,14 +6,16 @@ class Playlist {
   }
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   song(song1, song2, song3){
-    this.song1 = song1,
-    this.song2 = song2,
-    this.song3 = song3
+    [[this.song1 = song1,],
+    [this.song2 = song2,],
+    [this.song3 = song3]]
   }
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // favorites()
   // An addSong method that adds a song to the songs property.
-
+  addSong(song){
+    this.song = song().push
+  }
 }
 
 
@@ -23,8 +25,6 @@ const playlist = new Playlist("blink")
 // Create an Album class that inherits from Playlist. It should also...
 class Album extends Playlist {
   // Have an artist property that is determined by some input.
-  constructor(artist){
-    this.artist = artist
-  }
 }
 // Create an instance of the Album class.
+const california = new Album("b182")
