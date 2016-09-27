a57ffad3b9026cdc2fbd88abdc2f49038361530a
Mark Sweet <marks828@gmail.com>
2016-09-27 09:49:14 -0400

    filter added

--- fp.js
+++ fp.js
@@ -14,17 +14,19 @@ console.log(peoplesAges);
 
 // Convert the following code to use `.filter`
 
-// var peopleYoungerThan35 = []
-// var people = [
-//   {name: "Jane", age: 32},
-//   {name: "Jessica", age: 34},
-//   {name: "Jasmine", age: 39}
-// ]
-// people.forEach(person => {
-//   if(person.age < 35){
-//     peopleYoungerThan35.push(person)
-//   }
-// })
+var peopleYoungerThan35 = []
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+people.filter(person => {
+  if(person.age < 35){
+    peopleYoungerThan35.push(person.name, person.age)
+  }
+})
+
+console.log(peopleYoungerThan35);
 
 // Convert the following to use `.forEach`
 
