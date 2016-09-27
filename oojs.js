501ffd15940abbde672cc676f36ed57cb0c34f52
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-27 09:24:52 -0400

    add song

--- oojs.js
+++ oojs.js
@@ -5,9 +5,15 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
   music class Playlist {
+    constructor(songTitle, genre){
     this.songTitle = songTitle;
     this.genre = genre;
     this.songs = ["Achey Breaky Heart", "Bad to the Bone", "Miami"]
+    favorites = 0
+  },
+  addSong(addSong) {
+    this.songs.add(addSong);
+  },
 
 
   }
