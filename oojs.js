717c349a12bff7d3e1d3edd4029cc2a1fac3641d
Ian Hollands <ihollands@outlook.com>
2016-09-27 09:23:35 -0400

    created extender class Album and instantiated

--- oojs.js
+++ oojs.js
@@ -11,17 +11,11 @@ class Playlist {
 
 var hits = new Playlist("Indie Hits", "Ocean Eyes", "GFY");
 
+class Album extends Playlist {
+  constructor (title, song1, song2, artist) {
+  super(title, song1, song2);
+    this.artist = artist;
+  }
+}
 
-// Define a Playlist class. It should have...
-
-  // A title property that is determined by some input.
-  // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
-  // A favorites property that is initialized at 0. This cannot be set by user input.
-  // An addSong method that adds a song to the songs property.
-
-// Create an instance of the Playlist class.
-
-// Create an Album class that inherits from Playlist. It should also...
-  // Have an artist property that is determined by some input.
-
-// Create an instance of the Album class.
+var indieAlbum = new Album("Indie Hits", "Ocean Eyes", "GFY", "MGMT");
