f3c662e8314c93b0411bfffd9f50de749f0268ba
Mark Sweet <marks828@gmail.com>
2016-09-27 09:16:29 -0400

    created album that inherits from playlist

--- oojs.js
+++ oojs.js
@@ -6,18 +6,25 @@ class Playlist {
   }
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   song(song1, song2, song3){
-    this.song1 = song1
-    this.song2 = song2
+    this.song1 = song1,
+    this.song2 = song2,
     this.song3 = song3
   }
   // A favorites property that is initialized at 0. This cannot be set by user input.
+  // favorites()
   // An addSong method that adds a song to the songs property.
 
 }
 
+
+
 // Create an instance of the Playlist class.
-
+const playlist = new Playlist("blink")
 // Create an Album class that inherits from Playlist. It should also...
+class Album extends Playlist {
   // Have an artist property that is determined by some input.
-
+  constructor(artist){
+    this.artist = artist
+  }
+}
 // Create an instance of the Album class.
