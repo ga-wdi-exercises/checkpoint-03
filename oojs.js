69e15826ce84b4b1e5abeb47d67ee2511a825950
Barrett Cope <barrettcope@gmail.com>
2016-09-27 09:07:39 -0400

    Create Album class.

--- oojs.js
+++ oojs.js
@@ -21,4 +21,11 @@ var myPlaylist = new Playlist("The Best Songs",["Simply the best"]);
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
 
+class Album extends Playlist {
+  constructor(title,songs,artist) {
+    super(title,songs);
+    this.artist = artist;
+  }
+}
+
 // Create an instance of the Album class.
