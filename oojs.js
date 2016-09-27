9ee1fc51584ebdd301036d21963217a2a13d29e2
manish pradhan <pradhanmanish33@gmail.com>
2016-09-27 09:34:54 -0400

    commit2

--- oojs.js
+++ oojs.js
@@ -3,9 +3,20 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
+class Playlist {
+  constructor(title, songs, favorites) {
+    this.title = [];
+    this.songs = ["baby", "missing you", "only one"];
+    this.favorites = 0;
+  }
+  addSong(song){
+    this.songs.push("rainfall");
 
+  }
+}
+console.log(song);
 // Create an instance of the Playlist class.
-
+let playList = new Playlist("love song")
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
 
