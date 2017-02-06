b5ebd2bc3c9aabe9470297c61449ad932f2d6acc
Ebenezer Williams <ebenezerw@me.com>
2017-02-06 09:20:41 -0500

    created instance of playlist

--- oojs.js
+++ oojs.js
@@ -18,7 +18,11 @@
 
 // Create an instance of the Playlist class.
 
+let driving = new Playlist("Long Drive", "California Dreamin")
+
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
+
+  
 
 // Create an instance of the Album class.
