bf72f6ca46bf9dd899a6723fe2138f95f4471a7c
Elena Santana <fanelenan7@gmail.com>
2017-02-06 09:45:41 -0500

    .forEach, something not working though

--- fp.js
+++ fp.js
@@ -6,9 +6,9 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person){
-  peoplesAges.push(person.age)
-});
+// people.forEach(function(person){
+//   peoplesAges.push(person.age)
+// });
 
 var pplsAges = people.map(function(person){return person.age})
 console.log(eachAge)
@@ -20,11 +20,11 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
 var youngerThan35 = people.filter(function(person){return person.age < 35})
 console.log(youngerThan35)
 // Convert the following to use `.forEach`
@@ -34,4 +34,15 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+// var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+
+var peopleWithS = []
+people.forEach(function(person){
+  var split = person.split("")
+  for (i = 0, i < split.length, i++) {
+    if (split[i].indexOf("s") != -1) {
+      peopleWithS.push(person)
+    }
+  }
+})
+console.log(peopleWithS)
