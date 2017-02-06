8c7d78d874ee153d6940528f5c92936de290bf01
Nairul <awhitehurst4@gmail.com>
2017-02-06 09:27:00 -0500

    fp.js Questions 2 and 3

--- fp.js
+++ fp.js
@@ -11,17 +11,15 @@ var peoplesAges = people.map(function(person){
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
+function youngerThan35(value) {
+  return value >= 35
+}
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+var peopleYoungerThan35 = people.filter(youngerThan35)
 
 // Convert the following to use `.forEach`
 
@@ -30,4 +28,13 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+
+var peopleWhoseNamesHaveTheLetterS = []
+
+people.forEach(function() {
+peopleWHoseNamesHaveTheLetterS.push(function(person){
+  return person.name.match("s")
+})
+})
+
+//people.filter(function(person){return person.name.match("s")});
