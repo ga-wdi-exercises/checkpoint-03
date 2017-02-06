0b714ca70e0c037e859a777236e239f703f4a01e
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-02-06 09:58:48 -0500

    Converted code using .filter

--- fp.js
+++ fp.js
@@ -1,34 +1,42 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = [];
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-];
-// people.forEach(function(person){
-//   peoplesAges.push(person.age)
-// });
-
-peoplesAges = people.map(function(people){
-  var newArray = {};
-  newArray[people.name] = people.age;
-  return newArray;
-});
-
-// Convert the following code to use `.filter`
-
-// var peopleYoungerThan35 = [];
+// var peoplesAges = [];
 // var people = [
 //   {name: "Jane", age: 32},
 //   {name: "Jessica", age: 34},
 //   {name: "Jasmine", age: 39}
 // ];
+// people.forEach(function(person){
+//   peoplesAges.push(person.age)
+// });
+
+// peoplesAges = people.map(function(people){
+//   var newArray = {};
+//   newArray[people.name] = people.age;
+//   return newArray;
+// });
+
+// Convert the following code to use `.filter`
+
+var peopleYoungerThan35 = [];
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+];
 // people.forEach(function(person) {
 //   if(person.age < 35){
 //     peopleYoungerThan35.push(person);
 //   }
 // });
+people.filter = function isOldEnough(people){
+  for(name in people){
+    if(age < 35){
+      peopleYoungerThan35[name] = people[name];
+    }
+  }
+  return peopleYoungerThan35;
+};
 
 // Convert the following to use `.forEach`
 
