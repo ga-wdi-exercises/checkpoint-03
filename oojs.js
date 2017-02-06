8e20c08494c5eb48c34cfa401829e7b8186ee5f6
Nat Carter <n.goldman.carter@gmail.com>
2017-02-06 09:14:45 -0500

    added Playlist.favorites

--- oojs.js
+++ oojs.js
@@ -6,6 +6,7 @@ class Playlist {
     // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
     this.songs = [];
     // A favorites property that is initialized at 0. This cannot be set by user input.
+    this.favorites = 0;
     // An addSong method that adds a song to the songs property.
   }
 }
