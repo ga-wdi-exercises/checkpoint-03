1d89326061f11512549daebb4f847aa69f87f996
Erica Norby <ericanorby@gmail.com>
2017-02-06 09:31:13 -0500

    converted code to use .filter

--- fp.js
+++ fp.js
@@ -11,22 +11,26 @@ var people = [
 // });
 
 var peoplesAges = people.map(function(person){
-  return person.age
+  return person.age;
 })
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
+// var peopleYoungerThan35 = []
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+
+var peopleYoungerThan35 = people.filter(function(person){
+  return person.age < 35;
+})
 
 // Convert the following to use `.forEach`
 
