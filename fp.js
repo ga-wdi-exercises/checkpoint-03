b17537cb7364534a2098ab3b05647d92b52c3521
Dan Lin <dlin2102@gmail.com>
2017-02-06 09:58:37 -0500

    correction

--- fp.js
+++ fp.js
@@ -36,8 +36,10 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
+
 /*var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});*/
+var peopleWhoseNamesHaveTheLetterS = []
 people.forEach(function(person){
-  peoples.push(person.name.match("s"))
+  peopleWhoseNamesHaveTheLetterS.push(person.name.match("s"))
 
 })
