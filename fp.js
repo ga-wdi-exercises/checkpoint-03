404a139bb7a224c53a72dfd8c627ffbff457bcb0
Fiona <h.natapra@gmail.com>
2017-02-06 09:33:32 -0500

    FP initial commit

--- fp.js
+++ fp.js
@@ -6,9 +6,14 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(person => {
-  peoplesAges.push(person.age)
-})
+// people.forEach(person => {
+//   peoplesAges.push(person.age)
+// })
+
+// Answer:
+people.map(function(people){
+return people.age;
+ });
 
 // Convert the following code to use `.filter`
 
@@ -18,11 +23,16 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(person => {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-})
+// people.forEach(person => {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// })
+
+// Answer:
+people.filter(function(people) {
+  return people.age < 35
+});
 
 // Convert the following to use `.forEach`
 
@@ -32,3 +42,7 @@ var people = [
   {name: "Jasmine", age: 39}
 ]
 var peopleWhoseNamesHaveTheLetterS = people.filter(person => person.name.match("s"))
+
+// people.forEach(function(people){
+//   return people.name.match("s");
+// })
