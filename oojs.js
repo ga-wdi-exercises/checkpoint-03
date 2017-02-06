b6111077c2b7bb241509c31194b3f80340435973
Bill Bobbitt <wmbobbitt@gmail.com>
2017-02-06 09:06:14 -0500

    created class and instance

--- oojs.js
+++ oojs.js
@@ -4,7 +4,20 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
+class Playlist {
+  constructor(title) {
+    this.title = title
+    this.songs = []
+    this.favorites = 0
+  }
+  addSong(song) {
+    this.songs.push(song)
+  }
+}
+
 // Create an instance of the Playlist class.
+
+let popCrush = new Playlist('Pop Crush')
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
