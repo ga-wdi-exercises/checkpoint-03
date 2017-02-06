e3b9660e24e84d56f5a6570c2eebdd257e5c8871
Andy Flickinger <atflick@gmail.com>
2017-02-06 09:43:43 -0500

    fp q1 done

--- fp.js
+++ fp.js
@@ -1,14 +1,19 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
+
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person){
-  peoplesAges.push(person.age)
-});
+// people.forEach(function(person){
+//   peoplesAges.push(person.age)
+// });
+
+var peoplesAges = people.map(function(x){
+  return x.age
+})
+
 
 // Convert the following code to use `.filter`
 
