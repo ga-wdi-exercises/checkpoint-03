fff252851301173919c2fb3130da8f3f9b4c1221
KEVIN KELLER <keller_kevin@bah.com>
2017-02-06 09:45:23 -0500

    forEach

--- fp.js
+++ fp.js
@@ -8,7 +8,11 @@ var people = [
 ]
 people.forEach(function(person){
   peoplesAges.push(person.age)
+  // console.log(peoplesAges)
 });
+
+// people.map(peoplesAges)
+// console.log(people)
 
 // Convert the following code to use `.filter`
 
@@ -18,11 +22,14 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+
+// people.filter(function(age) {return peopleYoungerThan35.push(age)})
+// console.log(peopleYoungerThan35);
 
 // Convert the following to use `.forEach`
 
@@ -31,4 +38,11 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+// var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){
+//                                                       return person.name.match("s")
+//                                                     });
+
+people.forEach(function(person) {
+  console.log(person.name.match("s"));
+  return person.name.match("s");
+});
