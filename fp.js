e1385dc7f910c51734ce71084f7be8c36157575e
Travis Batts <Travisbatts6@gmail.com>
2017-02-06 09:48:46 -0500

    save

--- fp.js
+++ fp.js
@@ -1,15 +1,20 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
+var peoplesAges = [32,34,39];
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person){
-  peoplesAges.push(person.age)
-});
+// people.forEach(function(person){
+//   peoplesAges.map(person.age)
+// });
 
+// var peoplesAges = [1, 5, 10, 15];
+var peoplesAges = people.map(function(x) {
+   return x * 2;
+});
+console.log(peoplesAges)
 // Convert the following code to use `.filter`
 
 var peopleYoungerThan35 = []
@@ -18,12 +23,14 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
+people.filter(function(person) {
+  if(people.age < 35){
     peopleYoungerThan35.push(person)
   }
 });
-
+// naysayers.filter(function(naysayer){
+//   return naysayer.age > 300;
+// })
 // Convert the following to use `.forEach`
 
 var people = [
@@ -31,4 +38,4 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+var peopleWhoseNamesHaveTheLetterS = people.forEach(function(person){return person.name.match("s")});
--- oojs.js
+++ oojs.js
@@ -4,8 +4,28 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
+class playlist  {
+  this.title = title;
+  this.songs = []
+  this.favorites = 0;
+}
+addSong(name){
+  this.songs.push(name);
+  console.log(songs);
+}
+
+
 // Create an instance of the Playlist class.
 
+
+class Album {
+  super(addSong, title)
+  this.artist = [];
+}
+artist(musician){
+  this.artist.push(musician);
+  console.log(artist);
+}
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
 
