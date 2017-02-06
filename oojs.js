e6e2260f3e36c0b786de4ea6f1ac34acd31ae79f
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-06 09:17:05 -0500

    answer two

--- oojs.js
+++ oojs.js
@@ -21,6 +21,10 @@ class Playlist {
 }
 // Create an instance of the Playlist class.
 
+var list = ['masterOfPuppets', 'bloodAndThunder', 'ironMan']
+
+var instance = new Playlist('metalPlaylist', list)
+
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
 
