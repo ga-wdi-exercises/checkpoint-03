058238d8899b5913a2290b1a9713fa146b0cbf67
Justin Shin <jjs3b@virginia.edu>
2017-02-06 09:18:23 -0500

    filter

--- fp.js
+++ fp.js
@@ -19,11 +19,13 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+  peopleYoungerThan35 = people.filter((x,i)=>people[i].age<35)
+
 
 // Convert the following to use `.forEach`
 
