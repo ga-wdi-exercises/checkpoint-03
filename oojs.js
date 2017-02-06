ee51521de0d1c4201781628aa674aaa5a4a97e94
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-06 09:52:32 -0500

    updating all of my files forgot to fork them

--- oojs.js
+++ oojs.js
@@ -3,10 +3,22 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
+  var playlist = {
+      title:"The Blueprint",
+      songs:"The Ruler's Back, Takeover, Izzo(H.O.V.A.), Girls, Girls, Girls, Jigga That Nigga, U Don't Know, Hola' Hovito, Heart of the City (Ain't No Love), Never Change, Song Cry, All I Need, Renegade, Blueprint (Momma Loves Me), Breathe Easy (Lyrical Exercise), Girls, Girls, Girls (remix)",
+      favorites: (0)
+      addSong = "++";
+  };
+
+  //this addSongs one really confused me :(
 
 // Create an instance of the Playlist class.
+    var playlist = new playlist();
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
+  var class = new Artist(Playlist);
+
 
 // Create an instance of the Album class.
+  var album = new Album();
