49f42215c08e52815e24e7e0bb6c899190b377c9
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-06 13:51:27 -0500

    updating

--- oojs.js
+++ oojs.js
@@ -3,17 +3,19 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
-  var playlist = {
-      title:"The Blueprint",
-      songs:"The Ruler's Back, Takeover, Izzo(H.O.V.A.), Girls, Girls, Girls, Jigga That Nigga, U Don't Know, Hola' Hovito, Heart of the City (Ain't No Love), Never Change, Song Cry, All I Need, Renegade, Blueprint (Momma Loves Me), Breathe Easy (Lyrical Exercise), Girls, Girls, Girls (remix)",
-      favorites: (0)
-      addSong = "++";
-  };
+class Playlist {
+  constructor(title){
+    this.title = title;
+    this.songs = "The Ruler's Back, Takeover, Izzo(H.O.V.A.), Girls, Girls, Girls, Jigga That Nigga, U Don't Know, Hola' Hovito, Heart of the City (Ain't No Love), Never Change, Song Cry, All I Need, Renegade, Blueprint (Momma Loves Me), Breathe Easy (Lyrical Exercise), Girls, Girls, Girls (remix)",
+    this.favorites = (0)
+    this.addSong = "++";
+  }
+};
 
   //this addSongs one really confused me :(
 
 // Create an instance of the Playlist class.
-    var playlist = new playlist();
+    var blue = new Playlist("The Blueprint");
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
@@ -21,4 +23,4 @@
 
 
 // Create an instance of the Album class.
-  var album = new Album();
+  var blueprint = new Album("Album");
