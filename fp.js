247f3149533a96ff1502a4302d246b9a7f279467
Vince Campanale <vcampanale0294@gmail.com>
2017-02-06 09:34:24 -0500

    map and filter

--- fp.js
+++ fp.js
@@ -1,28 +1,34 @@
 // Convert the following code to use `.map`
 
 var peoplesAges = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.forEach(function(person){
-  peoplesAges.push(person.age)
-});
+peopleAges = people.map(person => person.age)
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ]
+// people.forEach(function(person){
+//   peoplesAges.push(person.age)
+// });
+
+
 
 // Convert the following code to use `.filter`
 
 var peopleYoungerThan35 = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+peopleYoungerThan35 = people.filter(person => person.age < 35);
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ]
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+
+
 
 // Convert the following to use `.forEach`
 
