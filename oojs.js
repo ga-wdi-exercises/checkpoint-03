4eb1e3649082ae2dd77a989cccae3908d7ea04fd
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-02-06 09:26:58 -0500

    Created Instance of Playlist

--- oojs.js
+++ oojs.js
@@ -3,19 +3,21 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
-  class Playlist{
-      constructor(title){
-        this.title = title;
-        songs = [];
-        favorites = 0;
-      }
-      addSong(title){
-        this.title.push(songs);
-        console.log(songs);
-      }
+class Playlist{
+  constructor(title){
+    this.title = title;
+    var favorites = 0;
   }
+  addSong(){
+    const songs = [];
+    songs.push(this.title);
+    console.log(songs);
+  }
+}
 
 // Create an instance of the Playlist class.
+const beatle1 = new Playlist("Yesterday");
+
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
