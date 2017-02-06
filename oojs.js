3e57eaecd1e6207af47f6a427d67d9c7f01683e4
Chris Wightman <wightman_christopher@bah.com>
2017-02-06 09:16:48 -0500

    Initial commit of Part I progress.

--- oojs.js
+++ oojs.js
@@ -10,3 +10,17 @@
   // Have an artist property that is determined by some input.
 
 // Create an instance of the Album class.
+
+class Playlist {
+	constructor(title, songs){
+		this.title = title;
+		this.songs = [];
+		this.favorites = favorites
+	}
+}
+
+class Album extends Playlist{
+	constructor(title, songs, artist){
+		this.artist = artist
+	}
+}
