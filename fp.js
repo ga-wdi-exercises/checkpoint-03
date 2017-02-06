5d6b37875820475bdd2c48055101f51310114cfd
Eva Lai <evalai.dc@gmail.com>
2017-02-06 09:49:41 -0500

    for each and filter function

--- fp.js
+++ fp.js
@@ -1,29 +1,23 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
 people.map(function(person){
-  peoplesAges.push(person.age)
+  return people.age
 });
-console.log(peoplesAges)
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
+people.filter(function(person) {
+  return people.age < 35
 });
-
 // Convert the following to use `.forEach`
 
 var people = [
@@ -31,4 +25,7 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+var peopleWithS= []
+var peopleWhoseNamesHaveTheLetterS = people.forEach(function(person){
+  let sNames = person.name.match("s")
+  peopleWithS.push(sNames)});
