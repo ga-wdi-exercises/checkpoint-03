fff7636c38149e054e664f54f36bbe8dd1cfb9e0
Andy Flickinger <atflick@gmail.com>
2017-02-06 09:28:33 -0500

    q1 done

--- oojs.js
+++ oojs.js
@@ -4,6 +4,18 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
+class Playlist {
+  constructor(title, songs) {
+    this.title = title
+    this.songs = songs
+    this.favorites = 0
+  }
+  addSong(song) {
+    this.songs.push(song)
+  }
+}
+
+
 // Create an instance of the Playlist class.
 
 // Create an Album class that inherits from Playlist. It should also...
