410b85f80c9c3607c1a4f3112818bb3b5c931a79
Samuel Clements <sdkclements@gmail.com>
2017-02-06 09:27:47 -0500

    got filter working

--- fp.js
+++ fp.js
@@ -1,29 +1,31 @@
-// Convert the following code to use `.map`
+// // Convert the following code to use `.map`
 
-var peoplesAges = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.map(function(person){
-  peoplesAges.push(person.age)
-});
-console.log(peoplesAges)
-
-// Convert the following code to use `.filter`
-
-// var peopleYoungerThan35 = []
+// var peoplesAges = []
 // var people = [
 //   {name: "Jane", age: 32},
 //   {name: "Jessica", age: 34},
 //   {name: "Jasmine", age: 39}
 // ]
-// people.forEach(function(person) {
-//   if(person.age < 35){
-//     peopleYoungerThan35.push(person)
-//   }
+// people.map(function(person){
+//   peoplesAges.push(person.age)
 // });
+// console.log(peoplesAges)
+
+// Convert the following code to use `.filter`
+
+// function peopleYoungerThan35(person) {
+//   return people.age < 35
+// }
+var people = [
+  {name: "Jane", age: 32},
+  {name: "Jessica", age: 34},
+  {name: "Jasmine", age: 39}
+]
+
+let filtered = people.filter(function(el){
+  el.age > 35})
+console.log(filtered)
+
 
 // Convert the following to use `.forEach`
 
