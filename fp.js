cfef00c59941ce9bb7529034ead293faf43ae2bb
Andy Flickinger <atflick@gmail.com>
2017-02-06 09:46:19 -0500

    fp q2 done

--- fp.js
+++ fp.js
@@ -10,24 +10,28 @@ var people = [
 //   peoplesAges.push(person.age)
 // });
 
-var peoplesAges = people.map(function(x){
+var peoplesAges = people.map(function(x) {
   return x.age
 })
 
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
+
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+
+var peopleYoungerThan35 = people.filter(function(x) {
+  return x.age < 35
+})
 
 // Convert the following to use `.forEach`
 
