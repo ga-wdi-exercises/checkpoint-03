a6b6f9a9f6efa735ec809bdba59700c7a4616bc4
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-02-06 09:44:59 -0500

    Converted code using .map

--- fp.js
+++ fp.js
@@ -1,34 +1,40 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = []
+var peoplesAges = [];
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
-]
-people.forEach(function(person){
-  peoplesAges.push(person.age)
+];
+// people.forEach(function(person){
+//   peoplesAges.push(person.age)
+// });
+
+peoplesAges = people.map(function(people){
+  var newArray = {};
+  newArray[people.name] = people.age;
+  return newArray;
 });
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// var peopleYoungerThan35 = [];
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ];
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person);
+//   }
+// });
 
 // Convert the following to use `.forEach`
 
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ];
+// var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s");});
