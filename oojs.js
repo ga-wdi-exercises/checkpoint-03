18e6f2d569f9d3f3a421a5768d76c815c015ad62
Elena Santana <fanelenan7@gmail.com>
2017-02-06 09:22:23 -0500

    added instance of Album

--- oojs.js
+++ oojs.js
@@ -1,9 +1,9 @@
 // Define a Playlist class. It should have...
 class Playlist {
-  constructor(title, songs, favorites){
+  constructor(title, songs){
     this.title = title
     this.songs = []
-    {this.favorites = 0}
+    favorites = 0
   }
   addSong(){
     this.songs.push(songs)
@@ -15,8 +15,15 @@ class Playlist {
   // An addSong method that adds a song to the songs property.
 
 // Create an instance of the Playlist class.
-
+var grinding = new Playlist("Emergency", "Swamp Thing")
+grinding.addSong("Disconnected")
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
-
+class Album extends Playlist {
+  constructor(title, songs, artist){
+    super(title, songs)
+    this.artist = artist
+  }
+}
 // Create an instance of the Album class.
+var blackout = new Album("Blackout", "Hero", "Pegboard Nerds")
