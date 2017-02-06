fe88e2a73860c668166d64e87c9df51ba9d7ec03
Eva Lai <evalai.dc@gmail.com>
2017-02-06 09:11:56 -0500

    added playlist class with add songs method

--- oojs.js
+++ oojs.js
@@ -3,7 +3,17 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
-
+class Playlist {
+  constructor (title, song){
+    this.title = title
+    this.song = []
+    this.favorites = 0
+  }
+  addSong(track){
+    this.song.push(track)
+    console.log(`${this.song} has been added to "songs".`);
+  }
+}
 // Create an instance of the Playlist class.
 
 // Create an Album class that inherits from Playlist. It should also...
