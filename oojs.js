77155b01d0237c387f097cfcc34a5474b4c70f4a
Kay Ehni <kay.ehnigmail.com>
2017-02-06 09:26:33 -0500

    added both classes

--- oojs.js
+++ oojs.js
@@ -4,6 +4,35 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
+class Playlist {
+  constructor(title,song){
+    this.title = title;
+    if (song.constructor === Array ){ //allows user to add multiple songs at once in an array
+    this.songs = song;}
+    else{
+    this.songs = [].push(song);
+    }
+  }
+  addSong(song){
+    this.songs = this.songs.push(song);
+  }
+}
+
+class Album extends Playlist {
+  constructor(title,song,artist){
+    super();
+    this.title = title;
+    if (song.constructor === Array ){ //allows user to add multiple songs at once in an array
+    this.songs = song;}
+    else{
+    this.songs = [].push(song);
+    }
+    this.favorites = [];
+    this.artist = artist;
+  }
+}
+
+
 // Create an instance of the Playlist class.
 
 // Create an Album class that inherits from Playlist. It should also...
