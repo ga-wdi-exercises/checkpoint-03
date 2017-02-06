b93f807a09cca099c15b3936cca3b83bca367377
Wes <wes.coffay@gmail.com>
2017-02-06 09:09:26 -0500

    question 1 complete

--- oojs.js
+++ oojs.js
@@ -1,4 +1,14 @@
 // Define a Playlist class. It should have...
+class Playlist {
+  constructor(title){
+    this.title = title
+    this.songs = []
+    this.favorites = 0
+  }
+  addSong(song) {
+    this.songs.push(song)
+  }
+}
   // A title property that is determined by some input.
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
