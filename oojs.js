304dee2fa9e75e70041acab4df0de9160901e7a9
Nat Carter <n.goldman.carter@gmail.com>
2017-02-06 09:12:39 -0500

    added title property

--- oojs.js
+++ oojs.js
@@ -1,11 +1,16 @@
-// Define a Playlist class. It should have...
-  // A title property that is determined by some input.
+
+
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 
+// Define a Playlist class. It should have...
 class Playlist {
-  
+  constructor(title) {
+    // A title property that is determined by some input.
+    this.title = title;
+
+  }
 }
 
 // Create an instance of the Playlist class.
