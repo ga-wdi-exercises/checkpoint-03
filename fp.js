ba15ed9e564bbf0d841731b092683d21d9fe3817
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-06 09:38:54 -0500

    son of answers

--- fp.js
+++ fp.js
@@ -19,11 +19,15 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+peopleYoungerThan35 = people.filter(function(inp){
+  return inp.age < 35
+})
+
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
 
 // Convert the following to use `.forEach`
 
