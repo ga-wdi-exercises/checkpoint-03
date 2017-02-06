129005250f52f1b5e55d6d05beac77255122bdea
Will Hawkins <willhawkins123@yahoo.com>
2017-02-06 09:41:39 -0500

    FP 1

--- fp.js
+++ fp.js
@@ -7,9 +7,8 @@ var people = [
   {name: "Jasmine", age: 39}
 ]
 
-peoplesAges = people.map(function(x) {
-  y = people[0].age
-  return y
+peoplesAges = people.map(function(person) {
+  return person.age
 });
 // people.forEach(function(person){
 //   peoplesAges.push(person.age)
@@ -17,17 +16,20 @@ peoplesAges = people.map(function(x) {
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// var peopleYoungerThan35 = []
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ]
+//
+// peopleYoungerThan35 = people.filter
+
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
 
 // Convert the following to use `.forEach`
 
