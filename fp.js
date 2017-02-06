1894f972e5e2d18bff04cd8044bfa76a7688441e
Nat Carter <n.goldman.carter@gmail.com>
2017-02-06 09:40:45 -0500

    completed .forEach() conversion

--- fp.js
+++ fp.js
@@ -25,9 +25,14 @@ var peopleYoungerThan35 = people.filter(function(person) {
 
 // Convert the following to use `.forEach`
 
+var peopleWhoseNamesHaveTheLetterS = [];
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+people.forEach(function(person) {
+  if (person.name.match("s")) {
+    peopleWhoseNamesHaveTheLetterS.push(person);
+  }
+});
