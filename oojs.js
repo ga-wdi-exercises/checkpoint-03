5d260e1fa068de6d38019cc39cbfd894e60b357e
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-08 09:24:30 -0400

    playlist class and instance

--- oojs.js
+++ oojs.js
@@ -10,3 +10,17 @@
   // Have an artist property that is determined by some input.
 
 // Create an instance of the Album class.
+class Playlist{
+  constructor (title, songs){
+    this.title = title;
+    this.songs = {song};
+    this.favorites = 0;
+}
+
+  addSong(song){
+    this.songs.push(song)
+
+}
+
+
+var playlist1 = new Playlist('pop', 'poppin song',)
