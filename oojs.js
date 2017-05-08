b46ad82653ed04986451ec028d38191ab20d6cea
amlak befekadu <amlakgirma@yahoo.com>
2017-05-08 09:10:36 -0400

    one to six respond

--- oojs.js
+++ oojs.js
@@ -1,9 +1,17 @@
 // Define a Playlist class. It should have...
+
   // A title property that is determined by some input.
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
-
+class Playlist = {
+  title: "author",
+  songs: "Thriller",
+  favorites: 0,
+  addSong function(){
+    songs:song
+  }
+}
 // Create an instance of the Playlist class.
 
 // Create an Album class that inherits from Playlist. It should also...
