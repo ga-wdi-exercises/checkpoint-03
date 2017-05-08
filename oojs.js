350ba6b0dfe74d66fd3c75bf79f69752c1279225
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-08 09:03:33 -0400

    initial commit

--- oojs.js
+++ oojs.js
@@ -3,6 +3,10 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
+class Playlist {
+  
+}
+
 
 // Create an instance of the Playlist class.
 
