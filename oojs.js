8406604212870de6c4d3ab3623ddab0769277761
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-08 09:18:14 -0400

    create instance of Playlist class, fix addSong method

--- oojs.js
+++ oojs.js
@@ -4,19 +4,20 @@
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
 class Playlist {
-  constructor(title, songs) {
+  constructor(title) {
     this.title = title;
-    this.song = song;
-    this.favorites = 0;
+    this.songs = [];
+    this.favorite = 0;
   }
 
-  addSong(){
-    song.
+  addSong(song){
+    this.songs.push(song)
   }
 }
 
 
 // Create an instance of the Playlist class.
+const happy = new Playlist("Happy Beats")
 
 // Create an Album class that inherits from Playlist. It should also...
   // Have an artist property that is determined by some input.
