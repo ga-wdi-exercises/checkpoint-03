697c350e4ffd94334d3d13ccf142847777c4bc07
Andres Arango <andresarangov17@gmail.com>
2017-05-08 09:10:29 -0400

    define class Playlist

--- oojs.js
+++ oojs.js
@@ -3,7 +3,16 @@
   // A songs property that is determined by some input. It should contain multiple songs, each of which can just be a song title.
   // A favorites property that is initialized at 0. This cannot be set by user input.
   // An addSong method that adds a song to the songs property.
-
+class Playlist {
+  constructor(title, songs) {
+    this.title = title;
+    this.songs = [];
+    this.favorites = [];
+  }
+  addSong(song) {
+    this.songs.push(song);
+  }
+}
 // Create an instance of the Playlist class.
 
 // Create an Album class that inherits from Playlist. It should also...
