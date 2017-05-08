fc2629ac0c988e21252d70e03f79121bcf2c6f04
Erin <erinedawson@gmail.com>
2017-05-08 09:52:15 -0400

    convert for filter and forEach

--- fp.js
+++ fp.js
@@ -1,25 +1,26 @@
 // Convert the following code to use `.map`
 
-var peoplesAges = people.map();
+
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ];
+var peoplesAges = people.map(people);
+
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
+
 var people = [
   {name: "Jane", age: 32},
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
-]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
+];
+let peopleYoungerThan35 = people.filter((people) => {
+  return people.age < 35;
 });
+
 
 // Convert the following to use `.forEach`
 
@@ -28,4 +29,6 @@ var people = [
   {name: "Jessica", age: 34},
   {name: "Jasmine", age: 39}
 ]
-var peopleWhoseNamesHaveTheLetterS = people.filter(function(person){return person.name.match("s")});
+people.forEach((s) => {
+  console.log("These people have an 's' in their names:" + s)
+};
