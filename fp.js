8915983a01fe49e1db14d377285cbd653a1aeb9f
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-08 09:38:49 -0400

    covert using .filter

--- fp.js
+++ fp.js
@@ -23,17 +23,22 @@ var peoplesAges =  people.map(function(person){
 
 // Convert the following code to use `.filter`
 
-var peopleYoungerThan35 = []
-var people = [
-  {name: "Jane", age: 32},
-  {name: "Jessica", age: 34},
-  {name: "Jasmine", age: 39}
-]
-people.forEach(function(person) {
-  if(person.age < 35){
-    peopleYoungerThan35.push(person)
-  }
-});
+// var peopleYoungerThan35 = []
+// var people = [
+//   {name: "Jane", age: 32},
+//   {name: "Jessica", age: 34},
+//   {name: "Jasmine", age: 39}
+// ]
+// people.forEach(function(person) {
+//   if(person.age < 35){
+//     peopleYoungerThan35.push(person)
+//   }
+// });
+
+var peopleYoungerThan35 = people.filter(function(person){
+  return person.age < 35;
+})
+
 
 
 // Convert the following to use `.forEach`
